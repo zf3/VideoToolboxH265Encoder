392bc8769ea94050401721376d2c2b7671e66f6e
tomisacat <wajmjjj@gmail.com>
2017-08-12 15:13:25 +0800

    Initial Commit

---
+++ VideoToolboxCompression/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoToolboxCompression
+//
+//  Created by tomisacat on 12/08/2017.
+//  Copyright © 2017 tomisacat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
